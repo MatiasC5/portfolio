ac28ddefda1e781521082b071ffc9ba901e9a4f5
MatiasC5 <cardosomatias1994@gmail.com>
2024-01-31 10:01:07 -0300

    name of projects in the projects.json file was fixed

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -4,22 +4,26 @@ import { projects } from "../../projects.json";
 export const Work = () => {
   return (
     <main className="h-screen items-center grid">
-      <section className="w-full grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] m-auto container">
+      <header className="font-semibold text-xl text-right mr-24 font-mono">
+        <Link to={"/"}>Home</Link>
+      </header>
+      <section className="w-full grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] m-auto container gap-8">
         {projects.map((project) => (
           <Link
             to={`https://matiasc5.github.io/${project.title}/`}
             target="_blank"
             rel="noreferrer"
           >
-            <article key={project.id}>
-              <h2 className="text-xl font-semibold"> {project.title}</h2>
+            <article
+              key={project.id}
+              className="flex flex-col items-center  w-96 h-96 "
+            >
+              <h2 className="text-xl font-semibold font-mono">
+                {" "}
+                {project.title}
+              </h2>
 
-              <img
-                src={project.image}
-                width={300}
-                height={300}
-                alt={project.title}
-              />
+              <img src={project.image} alt={project.title} />
             </article>
           </Link>
         ))}
